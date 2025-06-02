b1f9b92e8f5e40392085e7664a11b85be85e07b6
Md Faizan <mdfaizankurisaray@gmail.com>
2025-06-02 16:14:55 +0530

    3rd commit

---
+++ code3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int n = 5;
+int main() {
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=i; j++) {
+            cout << j <<" ";
+        }
+        cout <<endl;
+    }
+
+
+cout <<endl;
+//print i
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=i; j++) {
+            cout << i <<" ";
+        }
+        cout <<endl;
+    }
+
+
+}
